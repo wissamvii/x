9738127bd206f7f6998806bddf15b7d1f30cc4db
wissamvii <33107645+wissamvii@users.noreply.github.com>
2025-06-25 13:34:12 +0200

    Add files via upload

---
+++ Fabinglbsx.js
@@ -0,0 +1,4 @@
+let link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdn.statically.io/gh/wissamvii/x/main/basicallyBasic%20(5).css';
+document.head.appendChild(link);
